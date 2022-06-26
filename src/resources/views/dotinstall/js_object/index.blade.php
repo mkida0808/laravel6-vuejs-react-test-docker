18cd9373fe01eba15dc014d3d08a65c885121b86
mkida0808 <mkida0808@yahoo.co.jp>
2022-06-26 23:03:45 +0900

    JSオブジェクト編をスタート（ルーティング設定を含む）

---
+++ src/resources/views/dotinstall/js_object/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>JavaScript Object</title>
+</head>
+<body>
+    <p>JavaScript Object</p>
+    {{-- <script src="../../js/dotinstall/js_basic/main.js"></script> --}}
+    {{-- <script src="{{ asset('js/dotinstall/js_basic/main.js') }}"></script> --}}
+    <script src="{{ asset('js/dotinstall/js_object/main.js') }}"></script>
+</body>
+</html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -15,6 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// JS基本編
 Route::get('/dotinstall/js_basic', function () {
     return view('dotinstall.js_basic.index');
+});
+
+// JSオブジェクト編
+Route::get('/dotinstall/js_object', function () {
+    return view('dotinstall.js_object.index');
 });
