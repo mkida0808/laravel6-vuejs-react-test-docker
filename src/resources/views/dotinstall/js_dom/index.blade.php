3c15acae725ee8995e3e2b781fe5bf2abb5df2f2
mkida0808 <mkida0808@yahoo.co.jp>
2022-06-29 03:16:30 +0900

    jsのDOM操作の挙動確認スタート

---
+++ src/resources/views/dotinstall/js_dom/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>JavaScript DOM</title>
+</head>
+<body>
+    <p>JavaScript DOM</p>
+
+    <h1 id="target">ドットインストール</h1>
+    <p>こんにちは。こんにちは。こんにちは。</p>
+    <p>こんにちは。こんにちは。こんにちは。</p>
+    <p>こんにちは。こんにちは。こんにちは。</p>
+
+    <script src="{{ asset('js/dotinstall/js_dom/main.js') }}"></script>
+</body>
+</html>
