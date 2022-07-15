d77261984f940dbb1836c5dee1beb34d0cbc1531
mkida0808 <mkida0808@yahoo.co.jp>
2022-07-16 02:16:04 +0900

    jsモーダルウインドウのモーダルアニメーションのCSS追記、z-indexによるマスク、モーダルの要素重なり順序をCSSで指定

--- src/resources/views/dotinstall/js_modalwindow/index.blade.php
+++ src/resources/views/dotinstall/js_modalwindow/index.blade.php
@@ -6,18 +6,20 @@
     <link rel="stylesheet" href="{{ asset('css/dotinstall/js_modalwindow/styles.css') }}">
 </head>
 <body>
-    <div id="open">
-        詳細を見る
-    </div>
-
-    <div id="mask" class="hidden"></div>
-
     <section id="modal" class="hidden">
         <p>モーダルテスト。モーダルテスト。モーダルテスト。モーダルテスト。モーダルテスト。モーダルテスト。モーダルテスト。モーダルテスト。モーダルテスト。モーダルテスト。</p>
         <div id="close">
             閉じる
         </div>
     </section>
+    
+    <div id="mask" class="hidden"></div>
+
+    <div id="open">
+        詳細を見る
+    </div>
+
+
 
     <script src="{{ asset('js/dotinstall/js_modalwindow/main.js') }}"></script>
 </body>
