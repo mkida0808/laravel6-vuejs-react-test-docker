f4b001dcac49fda8e0052ff3c330da371b3e8641
mkida0808 <mkida0808@yahoo.co.jp>
2022-07-15 15:05:14 +0900

    js_intersection_observer_apiの挙動確認（処理実行されるタイミング、thresholdの指定、交差した要素の情報を取得)

--- src/public/js/dotinstall/js_intersection_observer_api/main.js
+++ src/public/js/dotinstall/js_intersection_observer_api/main.js
@@ -1,5 +1,15 @@
 'use strict';
 
 {
+    // Intersection Observer APIのデフォルト処理
+    const target = document.querySelector('img');
 
+    const observer = new IntersectionObserver((entries) => {
+        console.log(entries);
+    },
+    {
+        threshold: 0.2,
+    });
+
+    observer.observe(target);
 }
