aa19d8ed310f995c339ee97beddf91a4840567fb
mkida0808 <mkida0808@yahoo.co.jp>
2022-07-16 01:57:43 +0900

    jsモーダルウインドウのマークアップ素材、スタイル素材をコーディング

--- src/resources/views/dotinstall/js_modalwindow/index.blade.php
+++ src/resources/views/dotinstall/js_modalwindow/index.blade.php
@@ -3,8 +3,22 @@
 <head>
     <meta charset="utf-8">
     <title>Modal Window</title>
+    <link rel="stylesheet" href="{{ asset('css/dotinstall/js_modalwindow/styles.css') }}">
 </head>
 <body>
+    <div id="open">
+        詳細を見る
+    </div>
 
+    <div id="mask" class="hidden"></div>
+
+    <section id="modal" class="hidden">
+        <p>モーダルテスト。モーダルテスト。モーダルテスト。モーダルテスト。モーダルテスト。モーダルテスト。モーダルテスト。モーダルテスト。モーダルテスト。モーダルテスト。</p>
+        <div id="close">
+            閉じる
+        </div>
+    </section>
+
+    <script src="{{ asset('js/dotinstall/js_modalwindow/main.js') }}"></script>
 </body>
 </html>
