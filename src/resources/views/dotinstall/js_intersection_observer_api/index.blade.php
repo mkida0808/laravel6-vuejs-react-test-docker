f61b7e65d474f60970a197d90bc8f054a2c90714
mkida0808 <mkida0808@yahoo.co.jp>
2022-07-16 12:41:11 +0900

    js_intersection_observer_apiのCSS、テスト画像の新ディレクトリを作成（ディレクトリ階層を変更）

--- src/resources/views/dotinstall/js_intersection_observer_api/index.blade.php
+++ src/resources/views/dotinstall/js_intersection_observer_api/index.blade.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="utf-8">
     <title>My Site</title>
-    <link rel="stylesheet" href="{{ asset('css/js_intersection_observer_api/styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/dotinstall/js_intersection_observer_api/styles.css') }}">
 </head>
 <body>
     <h1>動物紹介</h1>
     <p>こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。</p>
     <p>こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。</p>
-    <img src="{{ asset('img/js_intersection_observer_api/bear.png') }}" width="200" height="200">
-    <img src="{{ asset('img/js_intersection_observer_api/lion.png') }}" width="200" height="200">
+    <img src="{{ asset('img/dotinstall/js_intersection_observer_api/bear.png') }}" width="200" height="200">
+    <img src="{{ asset('img/dotinstall/js_intersection_observer_api/lion.png') }}" width="200" height="200">
     <p>こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。</p>
-    <img src="{{ asset('img/js_intersection_observer_api/panda.png') }}" width="200" height="200">
+    <img src="{{ asset('img/dotinstall/js_intersection_observer_api/panda.png') }}" width="200" height="200">
     <p>こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。</p>
 
     <script src="{{ asset('js/dotinstall/js_intersection_observer_api/main.js') }}"></script>
