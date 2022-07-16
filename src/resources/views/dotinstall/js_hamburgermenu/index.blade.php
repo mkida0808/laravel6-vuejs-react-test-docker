b1fc732178d649266d4882ac113cc00a8d3494e3
mkida0808 <mkida0808@yahoo.co.jp>
2022-07-16 14:24:14 +0900

    jsハンバーガーメニューの基本マークアップ、スタイリング済み。ハンバーガーメニューの挙動確認スタート

---
+++ src/resources/views/dotinstall/js_hamburgermenu/index.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/dotinstall/js_hamburgermenu/styles.css') }}">
+    <title>My Site</title>
+</head>
+<body>
+    <p style="padding: 0 16px;">JSハンバーガーメニュー編</p>
+
+    <header>
+        <div class="logo">
+            <h1>LOGO</h1>
+        </div>
+
+        <div class="sp-menu">
+            <span class="material-icons" id="open">menu</span>
+        </div>
+    </header>
+
+    <div class="overlay">
+        <span class="material-icons" id="close">close</span>
+        <nav>
+            <ul>
+                <li><a href="#">Menu</a></li>
+                <li><a href="#">Menu</a></li>
+                <li><a href="#">Menu</a></li>
+            </ul>
+        </nav>
+    </div>
+
+    <main>
+        <p>こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。</p>
+        <p>こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。</p>
+    </main>
+
+    <script src="{{ asset('js/dotinstall/js_hamburgermanu/main.js') }}"></script>
+</body>
+</html>
