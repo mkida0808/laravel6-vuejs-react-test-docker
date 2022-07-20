4ed4dc30f147cb7429cae0560de0a1bcaf5222b1
mkida0808 <mkida0808@yahoo.co.jp>
2022-07-20 21:56:42 +0900

    js_tabmenuの基本マークアップ、CSSスタイリング済み、挙動確認スタート

---
+++ src/resources/views/dotinstall/js_tabmenu/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>Tab Menu</title>
+    <link rel="stylesheet" href="{{ asset('css/dotinstall/js_tabmenu/styles.css') }}">
+</head>
+<body>
+    <h1>JSタブメニュー編</h1>
+
+    <div class="container">
+        <ul class="menu">
+            <li><a href="#">サイトの概要</a></li>
+            <li><a href="#">サービス内容</a></li>
+            <li><a href="#">お問い合わせ</a></li>
+        </ul>
+        <section class="content">
+            サイトの概要。サイトの概要。サイトの概要。サイトの概要。サイトの概要。サイトの概要。サイトの概要。サイトの概要。サイトの概要。サイトの概要。サイトの概要。サイトの概要。サイトの概要。サイトの概要。サイトの概要。
+        </section>
+        <section class="content">
+            サービス内容。サービス内容。サービス内容。サービス内容。サービス内容。サービス内容。サービス内容。サービス内容。サービス内容。サービス内容。サービス内容。サービス内容。サービス内容。サービス内容。サービス内容。
+        </section>
+        <section class="content">
+            お問い合わせ。お問い合わせ。お問い合わせ。お問い合わせ。お問い合わせ。お問い合わせ。お問い合わせ。お問い合わせ。お問い合わせ。お問い合わせ。お問い合わせ。お問い合わせ。お問い合わせ。お問い合わせ。お問い合わせ。
+        </section>
+    </div>
+
+
+    <script src="{{ asset('js/dotinstall/js_tabmenu/main.js') }}"></script>
+</body>
+</html>
