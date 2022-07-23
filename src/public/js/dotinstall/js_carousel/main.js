b369dfb07b9eac58916ad2246725736138c60636
mkida0808 <mkida0808@yahoo.co.jp>
2022-07-23 22:21:06 +0900

    js_carouselのテスト画像A→B移動のみ実装、挙動確認

---
+++ src/public/js/dotinstall/js_carousel/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+{
+    const next = document.getElementById('next');
+    const ul = document.querySelector('ul');
+    const slides = ul.children;
+
+    next.addEventListener('click', () => {
+        const slideWidth = slides[0].getBoundingClientRect().width;
+        // ul.style.transform = 'translateX(-200px)';
+        ul.style.transform = `translateX(${-1 * slideWidth}px)`;
+    });
+}
