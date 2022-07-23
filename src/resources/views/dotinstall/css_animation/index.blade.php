c93df68727333ecc4a47a06e12b8c812989ff558
mkida0808 <mkida0808@yahoo.co.jp>
2022-07-24 03:20:47 +0900

    css_animationの基本マークアップ、CSSスタイリング済み。ホバー時に図形に丸みを帯びるように仮実装＆挙動確認

---
+++ src/resources/views/dotinstall/css_animation/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>My CSS Animation</title>
+    <link rel="stylesheet" href="{{ asset('css/dotinstall/css_animation/styles.css') }}">
+</head>
+<body>
+    <h1>CSSアニメーション編</h1>
+    <div class="box"></div>
+</body>
+</html>
