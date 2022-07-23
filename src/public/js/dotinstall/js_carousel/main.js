01b30b318f968cb23b06de357e91cdee9ada34e4
mkida0808 <mkida0808@yahoo.co.jp>
2022-07-23 22:31:34 +0900

    js_carouselのprev（戻る）のテスト画像スライド機能実装、挙動確認

--- src/public/js/dotinstall/js_carousel/main.js
+++ src/public/js/dotinstall/js_carousel/main.js
@@ -2,6 +2,7 @@
 
 {
     const next = document.getElementById('next');
+    const prev = document.getElementById('prev');
     const ul = document.querySelector('ul');
     const slides = ul.children;
     let currentIndex = 0;
@@ -10,6 +11,12 @@
         currentIndex++;
         const slideWidth = slides[0].getBoundingClientRect().width;
         // ul.style.transform = 'translateX(-200px)';
+        ul.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
+    });
+
+    prev.addEventListener('click', () => {
+        currentIndex--;
+        const slideWidth = slides[0].getBoundingClientRect().width;
         ul.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
     });
 }
