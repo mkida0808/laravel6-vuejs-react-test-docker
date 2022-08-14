ff8c090abfee7300b2ceec0f8dad54996b5e7783
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-15 05:44:50 +0900

    bladeファイルを親と子に分けて@section, @yieldでテンプレートを記述し直し。それに伴うCSSスタイル修正

--- src/resources/views/laravel6basicshoda/index.blade.php
+++ src/resources/views/laravel6basicshoda/index.blade.php
@@ -1,40 +1,17 @@
-<!DOCTYPE html>
-<html lang="ja">
+@extends('laravel6basicshoda.layouts.helloapp')
 
-<head>
-    <meta charset="UTF-8">
-    <title>Hello/Index</title>
-    <link rel="stylesheet" href="{{ asset('css/laravel6basicshoda/styles.css') }}">
-</head>
+@section('title', 'Index')
 
-<body>
-    <h1>Blade/Index</h1>
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
 
-    {{-- @isset($msg)
-        <p>こんにちは、{{ $msg }}さん！</p>
-    @else
-    <p>お名前を入力して下さい</p>
-    @endisset --}}
+@section('content')
+<p>ここが本文のコンテンツです。</p>
+<p>必要なだけ記述できます。</p>
+@endsection
 
-    {{-- <form method="post" action="/laravel6basicshoda">
-        @csrf
-        <input type="text" name="msg">
-        <input type="submit">
-    </form> --}}
-
-    <ol>
-        @foreach ($data as $item)
-            @if ($loop->first)
-                <p>※データ一覧</p>
-                <ul>
-            @endif
-            <li>No.{{ $loop->iteration }} . {{ $item }}</li>
-            @if ($loop->last)
-                </ul>
-                <p>-----ここまで</p>
-            @endif
-        @endforeach
-    </ol>
-</body>
-
-</html>
+@section('footer')
+copyright 2022 makoto
+@endsection
