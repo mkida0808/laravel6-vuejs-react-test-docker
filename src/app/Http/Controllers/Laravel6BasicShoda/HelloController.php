73cb980245e2e536c75fc4db3825cebae8a1aec4
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-15 19:28:04 +0900

    バリデーション設定時、入力エラーを全て一括で配列取得してテンプレート表示する

--- src/app/Http/Controllers/Laravel6BasicShoda/HelloController.php
+++ src/app/Http/Controllers/Laravel6BasicShoda/HelloController.php
@@ -10,6 +10,18 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        return view('laravel6basicshoda.index');
+        return view('laravel6basicshoda.index', ['msg' => 'フォーム入力：']);
+    }
+
+    public function post(Request $request)
+    {
+        $validate_rule = [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric|between:0, 150'
+        ];
+        $this->validate($request, $validate_rule);
+
+        return view('laravel6basicshoda.index', ['msg' => '正しく入力されました']);
     }
 }
--- src/resources/views/laravel6basicshoda/index.blade.php
+++ src/resources/views/laravel6basicshoda/index.blade.php
@@ -8,17 +8,26 @@
 @endsection
 
 @section('content')
-    <p>ここが本文のコンテンツです。</p>
-    <p>これは、<middleware>google.com</middleware>へのリンクです。</p>
-    <p>これは、<middleware>yahoo.co.jp</middleware>へのリンクです。</p>
-    {{-- <table>
-        @foreach ($dataFromMiddleware as $item)
-            <tr>
-                <th>{{ $item['name'] }}</th>
-                <td>{{ $item['mail'] }}</td>
-            </tr>
-        @endforeach
-    </table> --}}
+    <p>{{ $msg }}</p>
+    {{-- $errorsはバリデーション時に発生したエラー内容のオブジェクトが格納されている --}}
+    @if (count($errors) > 0)
+    <div>
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    <form action="/laravel6basicshoda" method="post">
+        <table>
+            @csrf
+            <tr><th>name: </th><td><input type="text" name="name" value="{{ old('name') }}"></td></tr>
+            <tr><th>mail: </th><td><input type="text" name="mail" value="{{ old('mail') }}"></td></tr>
+            <tr><th>age: </th><td><input type="text" name="age" value="{{ old('age') }}"></td></tr>
+            <tr><th></th><td><input type="submit" value="send"></td></tr>
+        </table>
+    </form>
 @endsection
 
 @section('footer')
