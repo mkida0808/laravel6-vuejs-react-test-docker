79d96855ed834037ad457971d4506b534aeba0fd
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-16 00:38:40 +0900

    Validatorインスタンスによるバリデーションでエラー内容を日本語化対応

--- src/app/Http/Controllers/Laravel6BasicShoda/HelloController.php
+++ src/app/Http/Controllers/Laravel6BasicShoda/HelloController.php
@@ -21,8 +21,26 @@ class HelloController extends Controller
         return view('laravel6basicshoda.index', ['msg' => $msg]);
     }
 
-    // public function post(Request $request)
-    // {
-    //     return view('laravel6basicshoda.index', ['msg' => $msg]);
-    // }
+    public function post(Request $request)
+    {
+        $rules = [
+            'name' => 'required',
+            'email' => 'email',
+            'age' => 'numeric|between:0, 150',
+        ];
+        $messages = [
+            'name.required' => '名前は必ず入力して下さい',
+            'email.email' => 'メールアドレスが必要です',
+            'age.numeric' => '年齢を整数で記入して下さい',
+            'age.between' => '年齢は0〜150の間で入力して下さい',
+        ];
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if ($validator->fails()) {
+            return redirect('/laravel6basicshoda')
+            ->withErrors($validator)
+            ->withInput();
+        }
+        return view('laravel6basicshoda.index', ['msg' => '正しく入力されました。']);
+    }
 }
