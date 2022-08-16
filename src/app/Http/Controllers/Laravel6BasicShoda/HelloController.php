2bd5b8b1a627b32cbf1da6e1e0e888ee00e40d91
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-16 19:03:39 +0900

    パラメーター結合（ここではクエリー文字列取得）によるDBデータ取得、表示

--- src/app/Http/Controllers/Laravel6BasicShoda/HelloController.php
+++ src/app/Http/Controllers/Laravel6BasicShoda/HelloController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Laravel6BasicShoda;
 
 use App\Http\Controllers\Controller;
-// use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 // use Illuminate\Http\Response;
 // use App\Http\Requests\Laravel6BasicShoda\HelloRequest;
 // use Validator;
@@ -11,9 +11,15 @@ use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $items = DB::select('SELECT * FROM people');
+        if (isset($request->id))
+        {
+            $param = ['id' => $request->id];
+            $items = DB::select('SELECT * FROM people WHERE id = :id', $param);
+        } else {
+            $items = DB::select('SELECT * FROM people');
+        }
         return view('laravel6basicshoda.index', ['items' => $items]);
     }
 }
--- src/resources/views/laravel6basicshoda/index.blade.php
+++ src/resources/views/laravel6basicshoda/index.blade.php
@@ -10,12 +10,14 @@
 @section('content')
     <table>
         <tr>
+            <th>Id</th>
             <th>Name</th>
             <th>Mail</th>
             <th>Age</th>
         </tr>
         @foreach ($items as $item)
             <tr>
+                <td>{{ $item->id }}</td>
                 <td>{{ $item->name }}</td>
                 <td>{{ $item->mail }}</td>
                 <td>{{ $item->age }}</td>
