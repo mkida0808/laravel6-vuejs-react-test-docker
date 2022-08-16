d88959d386dacd1b1ae355d8ece8a2e3708eae8a
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-16 14:48:20 +0900

    DBクラスのDB::select文で基本的なSQLを実行、テンプレート表示。それに伴うCSSスタイリング修正

--- src/app/Http/Controllers/Laravel6BasicShoda/HelloController.php
+++ src/app/Http/Controllers/Laravel6BasicShoda/HelloController.php
@@ -3,63 +3,17 @@
 namespace App\Http\Controllers\Laravel6BasicShoda;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 // use Illuminate\Http\Response;
-use App\Http\Requests\Laravel6BasicShoda\HelloRequest;
-use Validator;
+// use App\Http\Requests\Laravel6BasicShoda\HelloRequest;
+// use Validator;
+use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        $msg = $request->hasCookie('msg') ? 'Cookie: ' . $request->cookie('msg') : '※Cookieはありません';
-        return view('laravel6basicshoda.index', ['msg' => $msg]);
-
-        // $validator = Validator::make($request->query(), [
-        //     'id' => 'required',
-        //     'pass' => 'required',
-        // ]);
-        // $msg = $validator->fails() ? 'クエリーに問題があります。' : 'ID/PASSを受付けました。フォームを入力して下さい。';
-        // return view('laravel6basicshoda.index', ['msg' => $msg]);
-    }
-
-    public function post(Request $request)
-    {
-        $validate_rule = [
-            'msg' => 'required',
-        ];
-        $this->validate($request, $validate_rule);
-        $msg = $request->msg;
-        $response = response()->view('laravel6basicshoda.index', ['msg' => '[' . $msg . ']をクッキーに保存しました']);
-        $response->cookie('msg', $msg, 100);
-        return $response;
-
-        // $rules = [
-        //     'name' => 'required',
-        //     'email' => 'email',
-        //     'age' => 'numeric|between:0, 150',
-        // ];
-        // $messages = [
-        //     'name.required' => '名前は必ず入力して下さい',
-        //     'email.email' => 'メールアドレスが必要です',
-        //     'age.numeric' => '年齢を整数で記入して下さい',
-        //     'age.between' => '年齢は0〜150の間で入力して下さい',
-        // ];
-        // $validator = Validator::make($request->all(), $rules, $messages);
-
-        // $validator->sometimes('age', 'min:0', function ($input) {
-        //     return !is_int($input->age);
-        // });
-
-        // $validator->sometimes('age', 'max:200', function ($input) {
-        //     return !is_int($input->age);
-        // });
-
-        // if ($validator->fails()) {
-        //     return redirect('/laravel6basicshoda')
-        //     ->withErrors($validator)
-        //     ->withInput();
-        // }
-        // return view('laravel6basicshoda.index', ['msg' => '正しく入力されました。']);
+        $items = DB::select('SELECT * FROM people');
+        return view('laravel6basicshoda.index', ['items' => $items]);
     }
 }
--- src/resources/views/laravel6basicshoda/index.blade.php
+++ src/resources/views/laravel6basicshoda/index.blade.php
@@ -8,57 +8,20 @@
 @endsection
 
 @section('content')
-    <p>{{ $msg }}</p>
-    {{-- $errorsはバリデーション時に発生したエラー内容のオブジェクトが格納されている --}}
-    @if (count($errors) > 0)
-    <p>入力に問題があります。再入力して下さい。</p>
-    {{-- <div>
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div> --}}
-    @endif
-    <form action="/laravel6basicshoda" method="post">
-        <table>
-            @csrf
-            @error ('msg')
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Mail</th>
+            <th>Age</th>
+        </tr>
+        @foreach ($items as $item)
             <tr>
-                <th>ERROR</th>
-                <td>{{ $message }}</td>
+                <td>{{ $item->name }}</td>
+                <td>{{ $item->mail }}</td>
+                <td>{{ $item->age }}</td>
             </tr>
-            @enderror
-            <tr>
-                <th>Message: </th>
-                <td><input type="text" name="msg" value="{{ old('msg') }}"></>
-            </tr>
-
-            {{-- @error ('email')
-            <tr>
-                <th>ERROR</th>
-                <td>{{ $message }}</td>
-            </tr>
-            @enderror
-            <tr>
-                <th>mail: </th>
-                <td><input type="text" name="email" value="{{ old('email') }}"></td>
-            </tr>
-
-            @error ('age')
-            <tr>
-                <th>ERROR</th>
-                <td>{{ $message }}</td>
-            </tr>
-            @enderror
-            <tr>
-                <th>age: </th>
-                <td><input type="text" name="age" value="{{ old('age') }}"></td>
-            </tr> --}}
-
-            <tr><th></th><td><input type="submit" value="send"></td></tr>
-        </table>
-    </form>
+        @endforeach
+    </table>
 @endsection
 
 @section('footer')
