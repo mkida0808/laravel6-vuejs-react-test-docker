a608b45504f36654839696c1f19c27c0983ef425
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-16 23:41:50 +0900

    DB::tableクエリビルダによるwhere/orWhereメソッドによる複数の検索条件指定を挙動確認

--- src/app/Http/Controllers/Laravel6BasicShoda/HelloController.php
+++ src/app/Http/Controllers/Laravel6BasicShoda/HelloController.php
@@ -81,8 +81,14 @@ class HelloController extends Controller
 
     public function show(Request $request)
     {
-        $id = $request->id;
-        $items = DB::table('people')->where('id', '<', $id)->get();
+        // $id = $request->id;
+        $name = $request->name;
+        // $items = DB::table('people')->where('id', '<', $id)->get();
+        $items = DB::table('people')
+        ->where('name', 'like', '%' . $name . '%')
+        ->orWhere('mail', 'like', '%' . $name . '%')
+        ->get();
+        
         return view('laravel6basicshoda.show', ['items' => $items]);
     }
 }
