158cb165521ae98edd0d766ff95b8d75bbe60ed2
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-18 20:33:40 +0900

    simplePaginationメソッドでページャーを作成（コントローラー側）

--- src/app/Http/Controllers/Laravel6BasicShoda/HelloController.php
+++ src/app/Http/Controllers/Laravel6BasicShoda/HelloController.php
@@ -8,18 +8,23 @@ use Illuminate\Http\Request;
 // use App\Http\Requests\Laravel6BasicShoda\HelloRequest;
 // use Validator;
 use Illuminate\Support\Facades\DB;
+use App\Models\Laravel6BasicShoda\Person;
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        if (isset($request->id))
-        {
-            $items = DB::table('people')->where('id', $request->id)->orderBy('age', 'asc')->get();
-        } else {
-            $items = DB::table('people')->orderBy('age', 'asc')->get();
-        }
+        // $items = DB::table('people')->orderBy('age', 'asc')->simplePaginate(5);
+        $items = Person::orderBy('age', 'asc')->simplePaginate(5);
         return view('laravel6basicshoda.index', ['items' => $items]);
+        
+        // if (isset($request->id))
+        // {
+        //     $items = DB::table('people')->where('id', $request->id)->orderBy('age', 'asc')->get();
+        // } else {
+        //     $items = DB::table('people')->orderBy('age', 'asc')->get();
+        // }
+        // return view('laravel6basicshoda.index', ['items' => $items]);
     }
 
     public function post(Request $request)
--- src/resources/views/laravel6basicshoda/index.blade.php
+++ src/resources/views/laravel6basicshoda/index.blade.php
@@ -24,6 +24,7 @@
             </tr>
         @endforeach
     </table>
+    {{ $items->links() }}
 @endsection
 
 @section('footer')
