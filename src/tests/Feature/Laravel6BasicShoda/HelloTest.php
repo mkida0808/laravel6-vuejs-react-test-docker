7b1b7c176ec26ed2c9023e78f462fcfbfa7571b6
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-19 16:45:50 +0900

    ユニットテストのテストファイル

--- src/tests/Feature/Laravel6BasicShoda/HelloTest.php
+++ src/tests/Feature/Laravel6BasicShoda/HelloTest.php
@@ -5,9 +5,12 @@ namespace Tests\Feature\Laravel6BasicShoda;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class HelloTest extends TestCase
 {
+    use DatabaseMigrations;
     /**
      * A basic feature test example.
      *
@@ -21,6 +24,7 @@ class HelloTest extends TestCase
     // }
     public function testHello()
     {
+        // 値のチェック
         $this->assertTrue(true);
 
         $arr = [];
@@ -31,5 +35,20 @@ class HelloTest extends TestCase
 
         $n = random_int(0, 100);
         $this->assertLessThan(100, $n);
+
+        // アクセスチェック
+        $response = $this->get('/');
+        $response->assertStatus(200);
+
+        $response = $this->get('/laravel6basicshoda');
+        $response->assertStatus(302);
+
+        $user = factory(App\User::class)->create();
+        // var_dump($user);
+        $response = $this->actingAs($user)->get('/laravel6basicshoda');
+        $response->assertStatus(200);
+
+        $response = $this->get('/no_route');
+        $response->assertStatus(404);
     }
 }
