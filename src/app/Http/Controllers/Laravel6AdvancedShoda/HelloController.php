3cadbf7ea6a10f0df1c7cb32aee57b4032b808e7
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-20 23:54:53 +0900

    Storageクラスでファイルのコピー、移動、削除の挙動確認

--- src/app/Http/Controllers/Laravel6AdvancedShoda/HelloController.php
+++ src/app/Http/Controllers/Laravel6AdvancedShoda/HelloController.php
@@ -29,7 +29,14 @@ class HelloController extends Controller
 
     public function other($msg)
     {
-        Storage::disk('public')->prepend($this->fname, $msg);
+        if (Storage::disk('public')->exists('bk_' . $this->fname)) {
+            Storage::disk('public')->delete('bk_' . $this->fname);
+        }
+        Storage::disk('public')->copy($this->fname, 'bk_' . $this->fname);
+        if (Storage::disk('local')->exists('bk_' . $this->fname)) {
+            Storage::disk('local')->delete('bk_' . $this->fname);
+        }
+        Storage::disk('local')->move('public/bk_' . $this->fname, 'bk_' . $this->fname);
         return redirect()->route('hello');
     }
 }
