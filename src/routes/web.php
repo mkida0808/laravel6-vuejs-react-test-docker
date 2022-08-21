ef9a28c58bd466216c7ffcf0fac06101f6475eb7
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-21 16:30:50 +0900

    サービスコンテナとしてサービスのテストとしてクラスを作成、メソッドインジェクションの挙動確認

--- src/routes/web.php
+++ src/routes/web.php
@@ -59,10 +59,10 @@ Route::prefix('laravel6advancedshoda')->namespace('Laravel6AdvancedShoda')->grou
     // Route::middleware([HelloMiddleware::class])->group(function () {
         // Route::get('/hello/{id}', "HelloController@index")->where('id', '[0-9]+')->name('hello');
         Route::get('/hello', "HelloController@index")->name('hello');
-        Route::post('/hello', "HelloController@index");
-        Route::get('/hello/other', "HelloController@other");
-        Route::get('/hello/download', "HelloController@download");
-        Route::post('/hello/upload', "HelloController@upload");
+        // Route::post('/hello', "HelloController@index");
+        // Route::get('/hello/other', "HelloController@other");
+        // Route::get('/hello/download', "HelloController@download");
+        // Route::post('/hello/upload', "HelloController@upload");
     // });
 });
 // ********************* PHPフレームワークLaravel実践開発（ルーティング終了） *********************
