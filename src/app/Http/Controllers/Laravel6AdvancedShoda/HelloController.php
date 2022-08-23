d896ed797bbca2b623705754b6b9d569b0abfd4a
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-23 23:34:02 +0900

    DB::tableクラスでwhereInメソッドによる複数の値検索からレコード取得、画面表示を確認

--- src/app/Http/Controllers/Laravel6AdvancedShoda/HelloController.php
+++ src/app/Http/Controllers/Laravel6AdvancedShoda/HelloController.php
@@ -12,25 +12,10 @@ class HelloController extends Controller
 {
     public function index($id)
     {
-        $data = [
-            'msg' => '',
-            'data' => [],
-        ];
-        $msg = 'get: ';
-        $result = [];
-        $count = 0;
-        DB::table('people')->chunkById(3, function ($items) use (&$msg, &$result, &$id, &$count) {
-            if ($count == $id) {
-                foreach ($items as $item) {
-                    $msg .= $item->id . ':' . $item->name . ', ';
-                    $result += array_merge($result, [$item]);
-                }
-                return false;
-            }
-            $count++;
-            return true;
-        });
-        // $result = DB::table('people')->get();
+        $ids = explode(',', $id);  // $idsは配列になる
+        $msg = 'get people.';
+        // $result = DB::table('people')->whereBetween('id', $ids)->get();
+        $result = DB::table('people')->whereIn('id', $ids)->get();
         $data = [
             'msg' => $msg,
             'data' => $result,
