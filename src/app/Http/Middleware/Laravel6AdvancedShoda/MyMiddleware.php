a2ba382aa51aee7c9fd1f87ce11225479cd4523d
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-23 13:42:53 +0900

    MyMiddlewareクラスで前処理に加えて後処理を追加、挙動確認

--- src/app/Http/Middleware/Laravel6AdvancedShoda/MyMiddleware.php
+++ src/app/Http/Middleware/Laravel6AdvancedShoda/MyMiddleware.php
@@ -16,6 +16,7 @@ class MyMiddleware
      */
     public function handle($request, Closure $next)
     {
+        // 前処理開始
         $id = rand(0, count(MyService::alldata()));
         MyService::setId($id);
         $merge_data = [
@@ -24,6 +25,19 @@ class MyMiddleware
             'alldata' => MyService::alldata(),
         ];
         $request->merge($merge_data);
-        return $next($request);
+        // 前処理終了
+
+        // 後処理開始
+        $response = $next($request);
+        $content = $response->content();
+        $content .= '<style>
+        body { background-color: #eef; }
+        p { font-size: 18pt; }
+        li { color: red; font-wegiht: bold; }
+        </style>';
+        $response->setContent($content);
+        // 後処理終了
+
+        return $response;
     }
 }
