7a3df6145381b70022345dce9bbe2c46ecb8d7b7
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-26 02:28:04 +0900

    Scheduleクラスでコマンドを実行（バッチファイルを用意して簡易処理を実行）

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Models\Laravel6AdvancedShoda\Person;
+use App\Jobs\Laravel6AdvancedShoda\MyJob;
 
 class Kernel extends ConsoleKernel
 {
@@ -26,6 +28,14 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        // $schedule->exec('./mycmd.sh');
+        // $schedule->command('queue:work --stop-when-empty');
+        $count = Person::all()->count();
+        $id = rand(0, $count) + 1;
+        $schedule->call(function () use ($id) {
+            $person = Person::find($id);
+            MyJob::dispatch($person);
+        });
     }
 
     /**
