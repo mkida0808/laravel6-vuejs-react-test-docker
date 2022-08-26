dbbd03fb679c7df7fc9a7aed3719dec7867c4114
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-26 18:24:41 +0900

    HelloControllerのPHPユニットテストを実行

--- src/tests/Feature/ExampleTest.php
+++ src/tests/Feature/ExampleTest.php
@@ -14,8 +14,16 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        // $response = $this->get('/');
+        // $response->assertStatus(200);
+        $this->get('/laravel6advancedshoda')->assertStatus(404);
+        // $this->get('/laravel6advancedshoda/hello')->assertOk();
+        // $this->post('/laravel6advancedshoda/hello')->assertOk();
+        // $this->get('/laravel6advancedshoda/hello/1')->assertOk();
+        $this->get('/laravel6advancedshoda/hoge')->assertStatus(404);
+        $this->get('/laravel6advancedshoda/hello')->assertSeeText('Index');
+        // $this->get('/laravel6advancedshoda/hello')->assertSee('<h1>'); // メモリエラー
+        // $this->get('/laravel6advancedshoda/hello')->assertSeeInOrder(['<html', '<head', '<body', '<h1>']); // メモリエラー
+        
     }
 }
