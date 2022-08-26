e55faa4759534c225db551608cdb9dc51a0c2ec1
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-26 22:40:52 +0900

    モデルのユニットテストを動かそうとしているが動かない、後ほど再検証

--- src/tests/Feature/ExampleTest.php
+++ src/tests/Feature/ExampleTest.php
@@ -14,16 +14,28 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
+        $data = [
+            'id' => 1,
+            'person_id' => 1,
+            'title' => 'タイトル1',
+            'message' => 'メッセージ1',
+            'age' => 34,
+        ];
+        $this->assertDatabaseHas('board', $data);
+        // $data['id'] = 2;
+        // $this->assertDatabaseMissing('board', $data);
+
+        // $this->get('/laravel6advancedshoda')->assertStatus(404);
+        // $this->get('/laravel6advancedshoda/hoge')->assertStatus(404);
+        // $this->get('/laravel6advancedshoda/hello')->assertSeeText('Index');
+
         // $response = $this->get('/');
         // $response->assertStatus(200);
-        $this->get('/laravel6advancedshoda')->assertStatus(404);
         // $this->get('/laravel6advancedshoda/hello')->assertOk();
         // $this->post('/laravel6advancedshoda/hello')->assertOk();
         // $this->get('/laravel6advancedshoda/hello/1')->assertOk();
-        $this->get('/laravel6advancedshoda/hoge')->assertStatus(404);
-        $this->get('/laravel6advancedshoda/hello')->assertSeeText('Index');
         // $this->get('/laravel6advancedshoda/hello')->assertSee('<h1>'); // メモリエラー
         // $this->get('/laravel6advancedshoda/hello')->assertSeeInOrder(['<html', '<head', '<body', '<h1>']); // メモリエラー
-        
+
     }
 }
