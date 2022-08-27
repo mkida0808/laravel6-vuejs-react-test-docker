de318882afa44256d1041d9f9f27ba9d153ea0d4
mkida0808 <mkida0808@yahoo.co.jp>
2022-08-27 23:51:47 +0900

    ファクトリーでダミーデータを作成する際、データの条件成約をステートで指定実行、挙動確認

--- src/tests/Feature/ExampleTest.php
+++ src/tests/Feature/ExampleTest.php
@@ -17,71 +17,28 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        for ($i = 0; $i < 100; $i++) {
-            factory(Person::class)->create();
+        $list = [];
+        for ($i = 0; $i < 10; $i++)
+        {
+            $p1 = factory(Person::class)->create();
+            $p2 = factory(Person::class)->states('upper')->create();
+            $p3 = factory(Person::class)->states('lower')->create();
+            $p4 = factory(Person::class)->states('upper')->states('lower')->create();
+            $list = array_merge($list, [$p1->id, $p2->id, $p3->id, $p4->id]);
         }
-        $count = Person::get()->count();
-        $person = Person::find(rand(1, $count));
-        $data = $person->toArray();
-        print_r($data);
 
-        $this->assertDatabaseHas('people', $data);
+        for ($i = 0; $i < 10; $i++)
+        {
+            shuffle($list);
+            $item = array_shift($list);
+            $person = Person::find($item);
+            $data = $person->toArray();
+            print_r($data);
 
-        $person->delete();
-        $this->assertDatabaseMissing('people', $data);
+            $this->assertDatabaseHas('people', $data);
 
-        // $this->seed(DatabaseSeeder::class);
-        // $person = Person::find(1);
-        // $data = $person->toArray();
-
-        // $this->assertDatabaseHas('people', $data);
-
-        // $person->delete();
-        // $this->assertDatabaseMissing('people', $data);
-
-        // $person->delete();
-        // $this->assertDatabaseMissing('people', $data);
-
-        // $data = [
-        //     'id' => 21,
-        //     'name' => 'DUMMY',
-        //     'mail' => 'dummy@mail',
-        //     'age' => 0,
-        // ];
-        // $person = new Person();
-        // $person->fill($data)->save();
-        // $this->assertDatabaseHas('people', $data);
-
-        // $person->name = 'NOT-DUMMY';
-        // $person->save();
-        // $this->assertDatabaseMissing('people', $data);
-        // $data['name'] = 'NOT-DUMMY';
-        // $this->assertDatabaseHas('people', $data);
-
-        // $person->delete();
-        // $this->assertDatabaseMissing('people', $data);
-
-        // $data = [
-        //     'id' => 1,
-        //     'person_id' => 1,
-        //     'title' => 'タイトル1',
-        //     'message' => 'メッセージ1',
-        // ];
-        // $this->assertDatabaseHas('boards', $data);
-        // $data['id'] = 2;
-        // $this->assertDatabaseMissing('boards', $data);
-
-        // $this->get('/laravel6advancedshoda')->assertStatus(404);
-        // $this->get('/laravel6advancedshoda/hoge')->assertStatus(404);
-        // $this->get('/laravel6advancedshoda/hello')->assertSeeText('Index');
-
-        // $response = $this->get('/');
-        // $response->assertStatus(200);
-        // $this->get('/laravel6advancedshoda/hello')->assertOk();
-        // $this->post('/laravel6advancedshoda/hello')->assertOk();
-        // $this->get('/laravel6advancedshoda/hello/1')->assertOk();
-        // $this->get('/laravel6advancedshoda/hello')->assertSee('<h1>'); // メモリエラー
-        // $this->get('/laravel6advancedshoda/hello')->assertSeeInOrder(['<html', '<head', '<body', '<h1>']); // メモリエラー
-
+            $person->delete();
+            $this->assertDatabaseMissing('people', $data);
+        }
     }
 }
